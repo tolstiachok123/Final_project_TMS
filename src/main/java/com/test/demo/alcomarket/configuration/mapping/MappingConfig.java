bafdc31f5ddd2f0f0c2bc4774825bf4d9a7c4101
tolstiachok123 <sergey_andr1998@mail.ru>
2020-10-06 13:44:23 +0300

    Added dependency for ModelMapping library to pom.xml, created new package gor mapping configuration, changed mapping for AlcoholDrink and AlcoholDrinkDto.

---
+++ src/main/java/com/test/demo/alcomarket/configuration/mapping/MappingConfig.java
@@ -0,0 +1,22 @@
+package com.test.demo.alcomarket.configuration.mapping;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MappingConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+        return mapper;
+    }
+
+}
--- src/main/java/com/test/demo/alcomarket/mapper/AlcoholDrinkMapper.java
+++ src/main/java/com/test/demo/alcomarket/mapper/AlcoholDrinkMapper.java
@@ -2,31 +2,40 @@ package com.test.demo.alcomarket.mapper;
 
 import com.test.demo.alcomarket.dto.AlcoholDrinkDto;
 import com.test.demo.alcomarket.model.AlcoholDrink;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AlcoholDrinkMapper {
 
+    @Autowired
+    private ModelMapper mapper;
+
     public AlcoholDrink dtoToObject(AlcoholDrinkDto alcoholDrinkDto) {
-        AlcoholDrink alcoholDrink = new AlcoholDrink();
-        alcoholDrink.setAdv(alcoholDrinkDto.getAdv());
-        alcoholDrink.setCost(alcoholDrinkDto.getCost());
-        alcoholDrink.setName(alcoholDrinkDto.getName());
-        alcoholDrink.setPhotoPath(alcoholDrinkDto.getPhotoPath());
-        alcoholDrink.setQuantity(alcoholDrinkDto.getQuantity());
-        alcoholDrink.setType(alcoholDrinkDto.getType());
-        return alcoholDrink;
+//        AlcoholDrink alcoholDrink = new AlcoholDrink();
+//        alcoholDrink.setAdv(alcoholDrinkDto.getAdv());
+//        alcoholDrink.setCost(alcoholDrinkDto.getCost());
+//        alcoholDrink.setName(alcoholDrinkDto.getName());
+//        alcoholDrink.setPhotoPath(alcoholDrinkDto.getPhotoPath());
+//        alcoholDrink.setQuantity(alcoholDrinkDto.getQuantity());
+//        alcoholDrink.setType(alcoholDrinkDto.getType());
+//        return alcoholDrink;
+        return Objects.isNull(alcoholDrinkDto) ? null : mapper.map(alcoholDrinkDto, AlcoholDrink.class);
     }
 
     public AlcoholDrinkDto objectToDto(AlcoholDrink alcoholDrink) {
-        AlcoholDrinkDto alcoholDrinkDto = new AlcoholDrinkDto();
-        alcoholDrinkDto.setAdv(alcoholDrink.getAdv());
-        alcoholDrinkDto.setCost(alcoholDrink.getCost());
-        alcoholDrinkDto.setName(alcoholDrink.getName());
-        alcoholDrinkDto.setPhotoPath(alcoholDrink.getPhotoPath());
-        alcoholDrinkDto.setQuantity(alcoholDrink.getQuantity());
-        alcoholDrinkDto.setType(alcoholDrink.getType());
-        return alcoholDrinkDto;
+//        AlcoholDrinkDto alcoholDrinkDto = new AlcoholDrinkDto();
+//        alcoholDrinkDto.setAdv(alcoholDrink.getAdv());
+//        alcoholDrinkDto.setCost(alcoholDrink.getCost());
+//        alcoholDrinkDto.setName(alcoholDrink.getName());
+//        alcoholDrinkDto.setPhotoPath(alcoholDrink.getPhotoPath());
+//        alcoholDrinkDto.setQuantity(alcoholDrink.getQuantity());
+//        alcoholDrinkDto.setType(alcoholDrink.getType());
+//        return alcoholDrinkDto;
+        return Objects.isNull(alcoholDrink) ? null : mapper.map(alcoholDrink, AlcoholDrinkDto.class);
     }
 
 }
