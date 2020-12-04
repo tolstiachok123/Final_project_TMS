13d183826e6891006a08eae57ff5276116602121
tolstiachok123 <sergey_andr1998@mail.ru>
2020-12-04 19:53:44 +0300

    Add token creation to JwtTokenProvider and jwt dependency to pom.xml

--- src/main/java/com/test/demo/alcomarket/security/JwtTokenProvider.java
+++ src/main/java/com/test/demo/alcomarket/security/JwtTokenProvider.java
@@ -1,9 +1,59 @@
 package com.test.demo.alcomarket.security;
 
+import com.test.demo.alcomarket.model.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Component
 public class JwtTokenProvider {
 
+    @Value("springMyLove")
+    private String secretKey;
 
+    @Value("3600000")
+    private long validityInMilliseconds;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Qualifier("appUserDetailsService")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public String createToken(String username, List<Role> roles) {
+
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("roles", this.getRoleNames(roles));
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()//
+                .setClaims(claims)//
+                .setIssuedAt(now)//
+                .setExpiration(validity)//
+                .signWith(SignatureAlgorithm.HS256, secretKey)//
+                .compact();
+    }
+
+    private List<String> getRoleNames(List<Role> userRoles) {
+        List<String> rolesStringFormat = new ArrayList<>();
+
+        userRoles.forEach(role -> {
+            rolesStringFormat.add(role.getName().name());
+        });
+
+        return rolesStringFormat;
+    }
 }
